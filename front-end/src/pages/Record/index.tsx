d961b58432e2567184e19f27e6a5c540d245b007
kleberlimagit <mov_kleber@hotmail.com>
2021-01-14 17:45:53 -0300

    Record Table implements

--- front-end/src/pages/Record/index.tsx
+++ front-end/src/pages/Record/index.tsx
@@ -14,6 +14,31 @@ const Record = () =>{
                         <button className="button-submit">Ver Gráfico</button>
                     </form>
                 </div>
+
+                <table className="table-record" cellPadding="0" cellSpacing="0">
+                    <thead>
+                        <tr>
+                            <th>Instante</th>
+                            <th>Nome</th>
+                            <th>Idade</th>
+                            <th>Plataforma</th>
+                            <th>Gênero</th>
+                            <th>Título do game</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>20/08/2020 13:45</td>
+                            <td>João da Silva</td>
+                            <td>23</td>
+                            <td>Playstation</td>
+                            <td>Ação - Aventura</td>
+                            <td>The Last of Us 2</td>
+                        </tr>
+                    </tbody>
+                </table>
+
+
             </div>
         </div>
     );
