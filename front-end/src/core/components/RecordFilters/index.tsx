42207e510fb44005a2b8c0a8bfb11b0952c98b42
kleberlimagit <mov_kleber@hotmail.com>
2021-01-17 23:50:07 -0300

    Filter MaskedInput

--- front-end/src/core/components/RecordFilters/index.tsx
+++ front-end/src/core/components/RecordFilters/index.tsx
@@ -1,61 +1,84 @@
 
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import MaskedInput from 'react-text-mask'
+
 import './styles.scss'
 
 export type FilterForm = {
-    min?:string,
-    max?:string,
+    min?: string,
+    max?: string,
 }
 
 type Props = {
     onSearch: (filter: FilterForm) => void;
-    children?:React.ReactNode;
 }
 
 
-const RecordFilters = ({onSearch,children}:Props) => {
+const RecordFilters = ({ onSearch }: Props) => {
 
     const [minDate, setMinDate] = useState('');
     const [maxDate, setMaxDate] = useState('');
-    
 
-    const handleBlurMinDate = (min:string) => {
+
+    const handleBlurMinDate = (min: string) => {
         setMinDate(min)
-
-        onSearch({min, max: maxDate})
+        if(min.length ===10 || min.length === 0){
+            onSearch({ min, max: maxDate })
+        }
+        
     }
-    const handleBlurMaxDate = (max:string) => {
+    const handleBlurMaxDate = (max: string) => {
         setMaxDate(max)
+        if(max.length === 10 || max.length === 0){
+            onSearch({ min: minDate, max })
+        }
 
-        onSearch({min: minDate, max})
+        
     }
 
     const clearFilters = () => {
         setMinDate('')
         setMaxDate('');
 
-        onSearch({min:(''), max:('')})
+        onSearch({ min: (''), max: ('') })
     }
 
+
     return (
-        
+
         <div className="option-bar">
-                    
-                        <input type="text" name="min"   value={minDate} 
-                        onChange={event => handleBlurMinDate(event.target.value)}
-                        placeholder="Data Inicial"/>
+
+        <div>
+            <MaskedInput mask={[/\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/]}  
+                placeholder="Data inicial"
+                guide={false}
+                value={minDate} 
+                onChange={event => handleBlurMinDate(event.target.value)}
+                
+            />
+
+            <MaskedInput mask={[/\d/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/]} placeholder="Data Final"
+                value={maxDate}
+                guide={false}
+                onChange={event => handleBlurMaxDate(event.target.value)}
+            />
 
 
-                        <input type="text" name="max" value={maxDate} 
-                        onChange={event=> handleBlurMaxDate(event.target.value)}
-                        placeholder="Data Final"/>
-                        
-                        <button onClick={clearFilters}>
-                            Limpar Pesquisa
-                        </button>
+            <button onClick={clearFilters}>
+                Limpar Pesquisa
+            </button>
+        </div>
 
+        <div>
+            <Link to="/charts">
+                <button className="chart-record-button">
+                        Ver gráfico
+                </button>
+            </Link>
+        </div>
 
-                </div>
+        </div>
     );
 }
 
